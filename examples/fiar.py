9007bf9198bb53a74ce1e739fbbd996ecd7d316d
yu184743 <96573503+yu184743@users.noreply.github.com>
2025-07-10 21:37:12 +0900

    7/10

--- examples/fiar.py
+++ examples/fiar.py
@@ -11,7 +11,7 @@ from param_mc_remote import block
 # Connect to minecraft and open a session as player with origin location
 mc = Minecraft.create(address=param.ADRS_MCR, port=param.PORT_MCR)
 mc.setPlayer(param.PLAYER_NAME, PO.x, PO.y, PO.z)
-mc.postToChat("Hello, Minecraft Server!! from yu184743")
+# mc.postToChat("Hello, Minecraft Server!! from yu184743")
 mc.setBlock(5, 68, 5, block.GOLD_BLOCK)
 
 
@@ -39,14 +39,14 @@ CURSOR = (0, 123, 0)
 CTRL_KEYS = [[pygame.K_LEFT, "LEFT", -1],
              [pygame.K_RIGHT, "RIGHT", 1],
              [pygame.K_SPACE, "SPACE", 0],
-             [pygame.K_0, "WIN", 0], # デバッグ用 ---
+             [pygame.K_0, "WIN", 0], # デバッグ用　---
              [pygame.K_1, "BOARD", 0],
-             [pygame.K_2, "STONE", 0]] 
+             [pygame.K_2, "STONE", 0]]
 
 win_flag = False
 doCheck = True
 
-def win_check_old() : # 注：古いコード 後で振り返るために残す
+def win_check() : # 注：古いコード 後で振り返るために残す
     # 勝利判定文 print()はデバッグ用
     win_flag = True
     doCheck = True
@@ -101,58 +101,59 @@ def win_check_old() : # 注：古いコード 後で振り返るために残す
         if win_flag == True:
             doCheck = False
 
-def inBound(pos, n): # 盤内チェック 自作
-    if pos >= 81 or pos < 0 or cursor + n >= 9 or cursor - n < 0:
-        return False
-    else:
-        return True
-
-def win_check(): #自作 判定が怪しい 長い
+def win_check2(): #自作 判定が怪しい 長い
     wincount = 0
     for x in range(4):
-        if inBound(sta + 9*x, x) and color[sta + 9*x] == stonecolor: # 下
-            wincount += 1
-        else:
-            break
+        if sta + 9*x < 81: # 下
+            if color[sta + 9*x] == stonecolor:
+                wincount += 1
+            else:
+                break
     if wincount >= 4:
         return True
 
     wincount = 0
     for x in range(4):
-        if inBound(sta + x, x) and color[sta + x] == stonecolor: # 右
-            wincount += 1
-        else:
-            break
-        if inBound(sta - x, x) and color [sta - x] == stonecolor: # 左
-            wincount += 1
-        else:
-            break
+        if sta + x < (sta // 9 + 1) * 9: # 右
+            if color[sta + x] == stonecolor:
+                wincount += 1
+            else:
+                break
+        if sta - x >= (sta // 9) * 9: # 左
+            if color [sta - x] == stonecolor:
+                wincount += 1
+            else:
+                break
     if wincount >= 4:
         return True
 
     wincount = 0
     for x in range(4):
-        if inBound(sta - 8*x, x) and color[sta - 8*x] == stonecolor: # 右上
-            wincount += 1
-        else:
-            break
-        if inBound(sta + 8*x, x) and color[sta + 8*x] == stonecolor: # 左下
-            wincount += 1
-        else:
-            break
+        if sta - 8*x >= 0 and cursor + x < 9: # 右上
+            if color[sta - 8*x] == stonecolor:
+                wincount += 1
+            else:
+                break
+        if sta + 8*x < 81 and cursor - x > 0: # 左下
+            if color[sta + 8*x] == stonecolor:
+                wincount += 1
+            else:
+                break
     if wincount >= 4:
         return True
         
     wincount = 0
     for x in range(4):
-        if inBound(sta + 10*x, x) and color[sta + 10*x] == stonecolor: #右下
-            wincount += 1
-        else:
-            break
-        if inBound(sta - 10*x, x) and color[sta - 10*x] == stonecolor: # 左上
-            wincount += 1
-        else:
-            break
+        if sta + 10*x < 81 and cursor + x < 9: #右下
+            if color[sta + 10*x] == stonecolor:
+                wincount += 1
+            else:
+                break
+        if sta - 10*x >= 0 and cursor - x > 0: # 左上
+            if color[sta - 10*x] == stonecolor:
+                wincount += 1
+            else:
+                break
     if wincount >= 4:
         return True
     return False
@@ -165,11 +166,11 @@ def inBounds(x, y, board_size=BOARD_SIZE): #盤内チェック
     if x < 0 or y < 0 or x >= board_size or y >= board_size:
         return False
     else:
-        True
+        return True
 
 def checkWin(x, y, board, n=N):
     directions = [(1, 0), (0, 1), (1, 1), (1, -1)]
-    playerValue = board[x][y]
+    playerValue = stonecolor
     for dx, dy in directions:
         win_count = 1  # 中心の石
         
@@ -203,7 +204,7 @@ while running:
     whichturn = False
     # False = red, True = blue
 
-    board = [[0]*9]*9 # 二次元配列
+    boardcolor = [[0 for i in range(9)] for j in range(9)] # 二次元配列
     color = [0]*81 # 一次元配列
     stonecolor = 1
     # 0 = nothing, 1 = red, 2 = blue
@@ -241,16 +242,18 @@ while running:
                             sta = cursor+(8-stonecount[cursor])*9
                             if stonecount[cursor] < 9:
                                 color[sta] = stonecolor #一次元配列
-                                board[cursor][8-stonecount[cursor]] = stonecolor #二次元配列
+                                boardcolor[cursor][8-stonecount[cursor]] = stonecolor #二次元配列
+                                
+                                print(str(cursor) + ", " + str(8 - stonecount[cursor]))
+                                if checkWin(cursor, 8-stonecount[cursor], boardcolor) :
+                                    gamenow = False
+                                    time.sleep(2)
+                                '''
+                                if win_check2():
+                                    gamenow = False
+                                    time.sleep(2)
+                                '''
                                 stonecount[cursor] += 1
-                                if checkWin(cursor, 8-stonecount[cursor], board) :
-                                    gamenow = False
-                                    time.sleep(2)
-                                '''
-                                if win_check():
-                                    gamenow = False
-                                    time.sleep(2)
-                                '''
                                 if whichturn == False:
                                     whichturn = True
                                 else:
@@ -258,16 +261,18 @@ while running:
                             update_flag = True
                         elif key[1] == "WIN":
                             print("game end debug")
-                            win_flag = True
+                            gamenow = False
                         elif key[1] == "BOARD":
-                            for y in range(9):
-                                for x in range(9):
-                                    print(board[x][y])
-                                print("\n")
+                            for i in range(9):
+                                for j in range(9):
+                                    print(boardcolor[j][i], end = " ")
+                                print(" ")
                             update_flag = True
                         elif key[1] == "STONE":
-                            for x in range(9):
-                                print(stonecount[x])
+                            for i in range(9):
+                                print(stonecount[i], end = " ")
+                            print(" ")
+                            update_flag = True
             elif event.type == pygame.KEYUP:
                 for key in CTRL_KEYS:
                     if event.type == key[0]:
@@ -292,8 +297,9 @@ while running:
 
         screen.fill(ground) # background color
 
+
         '''
-        for x in range(81): # 四角を描くコード 一次元配列版
+        for x in range(81): # 四角を描くコード 一次元配列版 正常に動作
             y = x//9
             if color[x] == 0:
                 pygame.draw.rect(screen, WHITE, Rect(48 + x%9 * 32, 70 + y * 32, 24, 24))
@@ -302,15 +308,16 @@ while running:
             else:
                 pygame.draw.rect(screen, BLUE, Rect(48 + x%9 * 32, 70 + y * 32, 24, 24))
         '''
+
         for y in range(9):
             for x in range(9):
-                if board[x][y] == 0:
-                   pygame.draw.rect(screen, WHITE, Rect(48 + x * 32, 70 + y * 32, 24, 24))
-                elif board[x][y] == 1:
+                if boardcolor[x][y] == 0:
+                    pygame.draw.rect(screen, WHITE, Rect(48 + x * 32, 70 + y * 32, 24, 24))
+                elif boardcolor[x][y] == 1:
                     pygame.draw.rect(screen, RED, Rect(48 + x * 32, 70 + y * 32, 24, 24))
-                elif board[x][y] == 2:
+                elif boardcolor[x][y] == 2:
                     pygame.draw.rect(screen, BLUE, Rect(48 + x * 32, 70 + y * 32, 24, 24))
-                     
+                
         for x in range(81): # ブロック設置
             y1 = (2*(9-(x//9)))+66
             x1 = 5+((x%9)*2)
@@ -329,12 +336,6 @@ while running:
         text1, rect1 = font1.render(str(cursor), WHITE)
         rect1.center = (360, 360)
         screen.blit(text1, rect1)
-
-        if win_flag:
-            gamenow =  False
-            time.sleep(2)
-        
-                
 
 
         skip_frames += 1
